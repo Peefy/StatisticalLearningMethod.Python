1f3fcc7ac0c4048a3e7e141922beb7f3069888b1
Peefy <xpf6677@163.com>
2018-10-20 19:14:17 +0800

    Finish chapter 3

--- src/chapter4/chapter4.py
+++ src/chapter4/chapter4.py
@@ -1,15 +1,40 @@
 
-class Chapter1:
+class Chapter4:
+    """
+    第4章 朴素贝叶斯法
+    """
     def __init__(self):
+        """
+        第4章 朴素贝叶斯法
+        """
         pass
 
     def note(self):
-        print('第1章 ')
-
-chapter1 = Chapter1()
+        """
+        chapter4 note
+        """
+        print('第4章 朴素贝叶斯法')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        
+chapter4 = Chapter4()
 
 def main():
-    chapter1.note()
+    chapter4.note()
 
 if __name__ == '__main__':
     main()
