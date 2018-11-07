a3d6477549c1ea60cbea445eb2c712ffece439f0
Peefy <xpf6677@163.com>
2018-11-07 21:06:38 +0800

    Add chpater 7 folder

---
+++ src/chapter7/chapter7.py
@@ -0,0 +1,26 @@
+
+class Chapter7:
+    """
+    第7章 支持向量机
+    """
+    def __init__(self):
+        """
+        第7章 支持向量机
+        """
+        pass
+
+    def note(self):
+        """
+        chapter7 note
+        """
+        print('第7章 支持向量机')
+        print('')
+        print('')
+
+chapter7 = Chapter7()
+
+def main():
+    chapter7.note()
+
+if __name__ == '__main__':
+    main()
