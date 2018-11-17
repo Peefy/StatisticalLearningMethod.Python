412dd0025539a1928270af28ff589056b1eef8a5
Peefy <xpf6677@163.com>
2018-11-17 13:47:13 +0800

    update

---
+++ src/chapter8/chapter8.py
@@ -0,0 +1,55 @@
+
+class Chapter8:
+    """
+    第8章 提升方法
+    """
+    def __init__(self):
+        """
+        第8章 提升方法
+        """
+        pass
+
+    def note(self):
+        """
+        chapter8 note
+        """
+        print('第8章 提升方法')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+
+chapter8 = Chapter8()
+
+def main():
+    chapter8.note()
+
+if __name__ == '__main__':
+    main()
