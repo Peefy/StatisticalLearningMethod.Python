7041d925d97d8c07748eeea12dc2f97a2a25de44
Peefy <xpf6677@163.com>
2018-12-06 08:34:22 +0800

    Add demo

--- src/chapter2/perception.py
+++ src/chapter2/perception.py
@@ -2,6 +2,7 @@
 import numpy as _np
 import math as _math
 
+
 class Perception:
     """
     感知机
@@ -32,8 +33,53 @@ class Perception:
     def run(self, x):
         return [self.perception_func(self.w0, xx, self.b0) for xx in x] 
 
-class SKLearnPerception:
-    pass
+class SKLearnMLPClassifier:
+    def __init__(self):
+        pass
+    
+    def demo(self):
+        # 载入数据集合
+        from sklearn.datasets import load_digits
+        digits = load_digits()
+        # 数据标准化
+        from sklearn.preprocessing import StandardScaler
+        scaler = StandardScaler()
+        scaler.fit(digits.data)
+        x_scaled = scaler.transform(digits.data)
+        # 数据分类
+        x = x_scaled
+        y = digits.target
+        # 划分训练集、测试集：
+        from sklearn.model_selection import train_test_split
+        x_train,x_test,y_train,y_test=train_test_split(x,y)
+        # 调用sklearn，使用感知机预测：
+        from sklearn.neural_network import MLPClassifier
+        mlp = MLPClassifier(hidden_layer_sizes=(30,30,30),activation='logistic',max_iter=100)
+        mlp.fit(x_train,y_train)
+        # 进行预测，并观察效果：
+        from sklearn.metrics import classification_report 
+        predicted = mlp.predict(x_test) 
+        print(classification_report(y_test, predicted)) 
+        # 进行调参，并观察参数改变对预测效果的影响：
+        from sklearn.pipeline import Pipeline
+        from sklearn.model_selection import GridSearchCV       
+        pipeline = Pipeline([
+            ('mlp',MLPClassifier(hidden_layer_sizes=(30,30,30),max_iter=100))
+        ])
+        parameters = {
+            'mlp__activation': ('identity','logistic','tanh','relu'),
+            'mlp__solver': ('lbfgs','sgd','adam')
+        }
+        grid_search = GridSearchCV(pipeline, parameters,verbose=1,n_jobs=-1)
+        grid_search.fit(x_train, y_train)
+        print('最佳效果：%0.3f' % grid_search.best_score_)
+        print('最优参数：')
+        best_parameters = grid_search.best_estimator_.get_params()
+        for param_name in sorted(parameters.keys()):
+            print('\t%s: %r' % (param_name, best_parameters[param_name]))
+
+        predictions = grid_search.predict(x_test)
+        print(classification_report(y_test, predictions))
 
 def main():
     p = Perception()
